52266bc3197809827f6f6809cf9aff43cee7e9ce
Faystos <fayst_@bk.ru>
2019-03-23 01:01:18 +0300

    внес крайние правки

---
+++ js/script.js
@@ -0,0 +1,56 @@
+var link = document.querySelector(".writeToUs");
+var emailForm = document.querySelector(".popupContact");
+var popupContactClose = document.querySelector(".contactClose");
+var maps = document.querySelector(".ourLocation");
+var mapsPopup = document.querySelector(".mainFooterPopup");
+var mapsClose = document.querySelector(".mapsClose");
+var names = document.querySelector("[name=name]");
+var buys = document.querySelectorAll(".buyPopup");
+var buyForm = document.querySelector(".popupBasketForm")
+var buyFormClose = document.querySelector(".formBuyClose")
+
+buys.forEach(function(item){
+   item.addEventListener("click", function (evt) {
+   evt.preventDefault();
+   buyForm.classList.add("popupBasketFormOpen");
+});
+});
+
+buyFormClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  buyForm.classList.remove("popupBasketFormOpen");
+});
+
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  emailForm.classList.add("popupContactOpen");
+  names.focus();
+});
+
+popupContactClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  emailForm.classList.remove("popupContactOpen");
+});
+
+maps.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapsPopup.classList.add("mainFooterPopupOpen");
+});
+
+mapsClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapsPopup.classList.remove("mainFooterPopupOpen");
+});
+
+/*buys.forEach(function(item){
+   item.addEventListener("click", function (evt) {
+   evt.preventDefault();
+   buyForm.classList.add("popupBasketFormOpen");
+});
+});
+
+buyFormClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  buyForm.classList.remove("popupBasketFormOpen");
+}); */
